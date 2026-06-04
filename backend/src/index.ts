2a3a85479459bae97a1fd7fb254a7bae9a5229d2
Shubham9528 <81708211+Shubham9528@users.noreply.github.com>
2026-06-04 13:10:04 +0530

    backend change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ import cors from "cors";
 import Razorpay from "razorpay";
 import axios from "axios";
 
-const url = `https://ai-career-backend-q7xn.onrender.com`;
+const url = process.env.SERVER_URL!;
 const interval = 30000;
 
 function reloadWebsite() {
