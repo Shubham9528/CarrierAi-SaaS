0e990ab51aa56c5771162d7ce66128ec5dd36e53
Shubham9528 <81708211+Shubham9528@users.noreply.github.com>
2026-06-05 11:54:54 +0530

    chore(server): enable keep-alive mechanism

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,18 +13,18 @@ const interval = 30000;
 
 
 // Function to reload the website(for keeping the server alive on render.com)
-// function reloadWebsite() {
-//   axios
-//     .get(url)
-//     .then((response) => {
-//       console.log("website reloded");
-//     })
-//     .catch((error) => {
-//       console.error(`Error : ${error.message}`);
-//     });
-// }
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      console.error(`Error : ${error.message}`);
+    });
+}
 
-// setInterval(reloadWebsite, interval);
+setInterval(reloadWebsite, interval);
 
 
 
